1d9c0928b178b209a63035d3d0cd0c429207daf7
gofrywplynie29 <dawidmarkiewicz214@gmail.com>
2025-01-24 08:58:22 +0100

    aaaa

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -2,123 +2,123 @@ package endpoints
 
 import (
 	"net/http"
-  "strconv"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-  "github.com/goferpwlynie/goRestApi/users"
-  "github.com/goferpwlynie/goRestApi/jsonTools"
+	jsontools "github.com/goferpwlynie/goRestApi/jsonTools"
+	"github.com/goferpwlynie/goRestApi/users"
 )
 
-func handleIdParam(ctx *gin.Context) int{
-  Id, err := strconv.Atoi(ctx.Param("id"))
+func handleIdParam(ctx *gin.Context) int {
+	Id, err := strconv.Atoi(ctx.Param("id"))
 
-  if err != nil{
-    ctx.JSON(http.StatusBadRequest, gin.H{
-      "Error": "Wrong Id type",
-    })
-  }
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"Error": "Wrong Id type",
+		})
+	}
 
-  return Id
+	return Id
 }
 
-func GetUsersHandler(ctx *gin.Context){
-    ctx.JSON(http.StatusOK, users.Users)
+func GetUsersHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, users.Users)
 }
 
-func GetUserHandler(ctx *gin.Context){
-  id := handleIdParam(ctx)
+func GetUserHandler(ctx *gin.Context) {
+	id := handleIdParam(ctx)
 
-  for i, v := range users.Users{
-    if v.Id == id{
-      ctx.JSON(http.StatusOK, users.Users[i])
-      break
-    }
-  }
+	for i, v := range users.Users {
+		if v.Id == id {
+			ctx.JSON(http.StatusOK, users.Users[i])
+			break
+		}
+	}
 }
 
 func PostUsersHandler(ctx *gin.Context) {
-    var user users.User
+	var user users.User
 
-    err := ctx.ShouldBindJSON(&user)
-    if err != nil{
-      ctx.JSON(http.StatusBadRequest,gin.H{
-        "error": "wrong data structure",
-      })
-      return
-    }
+	err := ctx.ShouldBindJSON(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "wrong data structure",
+		})
+		return
+	}
 
-   users.Users = append(users.Users, user) 
-   jsontools.WriteJsonFile()
+	users.Users = append(users.Users, user)
+	go jsontools.WriteJsonFile()
 
-   ctx.JSON(http.StatusOK, gin.H{
-     "message": user,
-   })
-  }
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": user,
+	})
+}
 
 func DeleteUsersHandler(ctx *gin.Context) {
-  id := handleIdParam(ctx) 
+	id := handleIdParam(ctx)
 
-  for i, v := range users.Users{
-    if v.Id == id{
-      users.Users = append(users.Users[:i], users.Users[i+1:]...)
-      break
-    }
-  }
-    
-  jsontools.WriteJsonFile()
-  ctx.Status(http.StatusCreated)
+	for i, v := range users.Users {
+		if v.Id == id {
+			users.Users = append(users.Users[:i], users.Users[i+1:]...)
+			break
+		}
+	}
+
+	go jsontools.WriteJsonFile()
+	ctx.Status(http.StatusCreated)
 }
 
-func PutUserHandler(ctx *gin.Context){
-  id := handleIdParam(ctx)
+func PutUserHandler(ctx *gin.Context) {
+	id := handleIdParam(ctx)
 
-  var user users.User
+	var user users.User
 
-  err := ctx.ShouldBindJSON(&user)
+	err := ctx.ShouldBindJSON(&user)
 
-  if err != nil{
-    ctx.JSON(http.StatusBadRequest,gin.H{
-      "error": "wrong data structure",
-    })
-    return
-  }
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "wrong data structure",
+		})
+		return
+	}
 
-  user.Id = id
-  
-  for i, v := range users.Users{
-    if v.Id == id{
-      users.Users[i] = user
-    }
-  }
+	user.Id = id
 
-  ctx.Status(http.StatusNoContent)
+	for i, v := range users.Users {
+		if v.Id == id {
+			users.Users[i] = user
+		}
+	}
+
+	ctx.Status(http.StatusNoContent)
 }
 
-func PatchUserHandler(ctx *gin.Context){
-  id := handleIdParam(ctx)
-  
-  var user users.User
+func PatchUserHandler(ctx *gin.Context) {
+	id := handleIdParam(ctx)
 
-  err := ctx.ShouldBindJSON(&user)
-  
-  if err != nil{
-    ctx.JSON(http.StatusBadRequest,gin.H{
-      "error": "wrong data structure",
-    })
-    return
-  }
+	var user users.User
 
-  for i, v := range users.Users{
-    if v.Id == id{
-      if user.Name != ""{
-        users.Users[i].Name = user.Name
-      }
-      if user.Surname != ""{
-        users.Users[i].Surname = user.Surname
-      }
-      if user.BirthYear != 0{
-        users.Users[i].BirthYear = user.BirthYear
-      }
-    }
-  }
+	err := ctx.ShouldBindJSON(&user)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "wrong data structure",
+		})
+		return
+	}
+
+	for i, v := range users.Users {
+		if v.Id == id {
+			if user.Name != "" {
+				users.Users[i].Name = user.Name
+			}
+			if user.Surname != "" {
+				users.Users[i].Surname = user.Surname
+			}
+			if user.BirthYear != 0 {
+				users.Users[i].BirthYear = user.BirthYear
+			}
+		}
+	}
 }
